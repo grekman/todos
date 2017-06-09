8221efac567d663654c5aedb7548dcddc61edb1c
grekman <grekman@gmail.com>
2017-06-09 09:19:55 +0300

    first stage

--- js/app.js
+++ js/app.js
@@ -1,64 +1,82 @@
-var info = [{
-  "author": "Gorbachev",
-  "bookname": "bla-bla-bla",
-  "pages": "542",
-  "img" : "https://images-na.ssl-images-amazon.com/images/I/61nPVlgFr4L._SX491_BO1,204,203,200_.jpg"
+$(document).ready(function() {});
+var todo = [{
+  "task" : "task 1",
+  "time" : "Total time is"
 },
 {
-  "author": "Vergeles",
-  "bookname": "do nothing",
-  "pages": "842",
-  "img" : "https://keithmurnighan.files.wordpress.com/2012/03/do-nothing-cover3d_400px.gif?w=800&h=608"
+  "task" : "task 2",
+  "time" : "Total time is"
 },
 {
-  "author": "Asyaleycum",
-  "bookname": "Go Forward",
-  "pages": "442",
-  "img" : "http://www.hoffan.org/wp-content/uploads/2015/12/GoForward-book-Front-cover.jpg"
+  "task" : "task 3",
+  "time" : "Total time is"
 }]
 
 
-var Books = React.createClass({
+var Task = React.createClass({
+  propTypes:{
+    data: React.PropTypes.shape({
+      task: React.PropTypes.string.isRequired,
+      time: React.PropTypes.string.isRequired
+    })
+  },
+  render: function() {
+    var task = this.props.data.task,
+        time = this.props.data.time;
+    return (
+      <div>
+        <input type="checkbox" className="filled-in" id={task} style={{display:"inline-block"}}/>
+        <label htmlFor={task}>{task}</label>
+        <p style={{display:"inline-block", marginLeft:"20px"}}>{time}</p>
+      </div>
+    )
+  }
+})
+
+var Todo = React.createClass({
   render: function() {
     var data = this.props.data;
-    var newsTemplate;
+    var todoTemplate;
 
-    if (data.length>0) {
-    newsTemplate = data.map(function(item, index) {
-      return(
-        <div key={index}>
-
-          <img src= {item.img}/>
-          <p className="books__author"> Автор: {item.author}</p>
-          <p className="books__bookname"> Название книги: {item.bookname}</p>
-          <p className="books__pages"> страниц {item.pages}</p>
-        </div>
+    if (data.length > 0) {
+      todoTemplate = data.map(function(item, index){
+        return (
+          <li key={index} className="collection-item">
+            <Task data={item}/>
+          </li>
+        )
+      });
+      return (
+        <ul className="collection">
+          {todoTemplate}
+        </ul>
       )
-    })
-  } else {
-    newsTemplate = <p> Книг нет </p>
+    }
   }
+})
 
+var NewTodo = React.createClass({
+  render: function() {
     return (
-      <div className="books">
-        <p className= {data.length > 0 ? 'bookprrsent':'booknone'}> Книг: {data.length}</p>
-        {newsTemplate}
-       </div>
-    );
+              <div className="input-field">
+                <input id="task" type="text" className="validate"/>
+                <label htmlFor="task" >What should be done?</label>
+              </div>
+    )
   }
-});
-
+})
 
 var App = React.createClass({
   render: function() {
-
     return (
-      <div>
-      <Books data={info}/>
+      <div className="main col l6 m8 s12 z-depth-5">
+        <h1>Create your plans</h1>
+          <NewTodo/>
+          <Todo data={todo}/>
       </div>
-    );
+    )
   }
-});
+})
 
 ReactDOM.render(
   <App/>,
